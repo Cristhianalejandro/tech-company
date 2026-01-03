f666b65cc7d86678fcc980f04fb7bc872e20a7f7
cristhian <cristhiandelgado0306@gmail.com>
2026-01-03 17:13:12 -0500

    fourth commit

--- src/components/ProcessSection.jsx
+++ src/components/ProcessSection.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import {colors,steps} from '../assets/data'
+ 
 function ProcessSection() {
   return (
-    <div>
+    <section id='services' className={`${colors.BORDER_STYLE} pt-12 sm:pt-16`}>
       
+    <div className="flex flex-col md:flex-row border border-gray-300 rounded-xl overflow-hidden bg-white min-h-[600px]">
+        <div className="md:w-1/4 p-6 sm:p-10 relative bg-gray-100 border-r border-gray-300 flex flex-col justify-between">
+             <div className="hidden md:block absolute inset-y-0 left-0 w-8 lg:w-12 pt-16 -ml-4 transform -rotate-90 origin-top-left">
+
+                  <span className='text-7xl font-extrabold tracking-widest '></span>
+
+             </div>
+        </div>
     </div>
+
+    </section>
   )
 }
 
