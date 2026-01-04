c96ba0dafa009e92bb9414bf05b9fb9fa47fdc9b
Cristhianalejandro <cristhiandelgado0306@gmail.com>
2026-01-04 17:19:53 -0500

    fith commit

--- src/components/ProcessSection.jsx
+++ src/components/ProcessSection.jsx
@@ -9,8 +9,13 @@ function ProcessSection() {
         <div className="md:w-1/4 p-6 sm:p-10 relative bg-gray-100 border-r border-gray-300 flex flex-col justify-between">
              <div className="hidden md:block absolute inset-y-0 left-0 w-8 lg:w-12 pt-16 -ml-4 transform -rotate-90 origin-top-left">
 
-                  <span className='text-7xl font-extrabold tracking-widest '></span>
+                  <span className='text-7xl font-extrabold tracking-widest '>SYSTEM</span>
 
+             </div>
+
+
+             <div className="relative z-10">
+                <p className="text-sm code-font tracking-widest uppercase mb-3 text-gray-600">OUR BLUEPRINT</p>
              </div>
         </div>
     </div>
